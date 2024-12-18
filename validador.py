a0252cf7d6bff777f64fa0d7e37e30af8d7175c9
jvcss <jvcs.mecatronica@gmail.com>
2024-12-18 15:09:06 -0300

    fix validador

--- validador.py
+++ validador.py
@@ -5,6 +5,8 @@ import os
 def read_csv(file_path):
     with open(file_path, newline='') as csvfile:
         reader = csv.reader(csvfile)
+        # Ignora o cabeçalho
+        next(reader)
         data = [list(map(int, row)) for row in reader]
     return data
 
@@ -28,8 +30,14 @@ def main(arg_out_number, arg_out_file, arg_out_search: str):
     """
     file_path = arg_out_file
     search_numbers = list(map(int, arg_out_search.split(",")))
-    sequences = read_csv(file_path)
+    sequences = read_csv(file_path)[1:] # Ignora o cabeçalho
     results = search_sequences(sequences, search_numbers)
+
+    start_win_number =  0
+    if len(search_numbers ) == 6:
+           start_win_number = 3 
+    elif len(search_numbers ) == 15:
+        start_win_number = 11
     count = Counter([match_count for _, match_count in results if match_count >= 3])
     count_2 = Counter([match_count for _, match_count in results if match_count > 1])
     for seq, match_count in results:
@@ -37,8 +45,10 @@ def main(arg_out_number, arg_out_file, arg_out_search: str):
             print(f"( {sum(seq)} ) Sequência: {seq} - Contagem de Acertos: {match_count}")
             continue
 
-    print(f"\nTotal de sequências com acertos: {count.__str__()}")
-    print(f"Total de sequências com acertos de tudo: {count_2.__str__()}\n")
+    print(f"\nTotal de sequências com acertos: \t{count.__str__()}")
+    print(f"Total de sequências com acertos de tudo: \t{count_2.__str__()}\n")
+
 if __name__ == "__main__":
     os.system('cls' if os.name == 'nt' else 'clear')
-    main(13, "C:\\Users\\vitim\\tutorial\\resultado_lotofacil.csv", "5,8,10,11,12,14,15,16,17,18,20,21,23,24,25")
+    resultados_gerados_por_ia = "resultado.csv"
+    main(6, resultados_gerados_por_ia, "2,11,21,30,35,52")
